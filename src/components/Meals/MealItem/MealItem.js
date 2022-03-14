32b5ed1a853db6652a398469a6841430d88c7d1b
togenius1 <90688382+togenius1@users.noreply.github.com>
2022-03-14 16:24:00 +0700

    added components

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import classes from './MealItem.module.css';
+
+export const MealItem = (props) => {
+    const price = `$${props.price.toFixed(2)}`;
+
+    return (
+        <li className={classes.meal}>
+            <div>
+                <h3>{props.name}</h3>
+                <div className={classes.description}>
+                    <h3>{props.description}</h3>
+                </div>
+                <div className={classes.price}>
+                    <h3>{price}</h3>
+                </div>
+            </div>
+            <div></div>
+        </li>
+    );
+};
---
+++ src/components/UI/Input.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './Input.module.css';
+
+function Input() {
+    return <div>Input</div>;
+}
+
+export default Input;
